f53fd2b6592289ae95f283a6c44df31040d23385
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2023-01-06 11:29:36 +0100

    :zap: add try catch for get posts on home page

--- src/routes/getRoot.ts
+++ src/routes/getRoot.ts
@@ -6,24 +6,29 @@ import { prisma } from "../../repositories/prisma"
 export function getRoot (app: Application) {
   app.get('/', async (req, res) => {
     var posts = Array()
-    const postsDB = await getAllPosts()
-    if(!postsDB) {
-      res.render(path.join(__dirname, '../views/index.ejs'))
-      return
+    try {
+      const postsDB = await getAllPosts()
+      if(!postsDB) {
+        res.render(path.join(__dirname, '../views/index.ejs'))
+        return
+      }
+      postsDB.forEach(async (post) => {
+        posts.push({
+          author: await getAuthorNameByPostId(post.id),
+          content: post.content,
+          createdAt: post.createdAt
+        })
+      })
+      setTimeout(function() {
+        posts.sort(function (a, b) {
+          return b.createdAt - a.createdAt
+        })
+        res.render(path.join(__dirname, '../views/index.ejs'), {posts})
+      }, 100) // add timeout to have time to get author name on DB
+    } catch (e) {
+      console.error(e)
+      res.status(500).send('Internal Server Error')
     }
-    postsDB.forEach(async (post) => {
-      posts.push({
-        author: await getAuthorNameByPostId(post.id),
-        content: post.content,
-        createdAt: post.createdAt
-      })
-    })
-    setTimeout(function() {
-      posts.sort(function (a, b) {
-        return b.createdAt - a.createdAt
-      })
-      res.render(path.join(__dirname, '../views/index.ejs'), {posts})
-    }, 100) // add timeout to have time to get author name on DB
     
   })
 }
